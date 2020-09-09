b18f6765388ca5ed48ac305a190711321cd70492
letJamie <stillbut@naver.com>
2020-09-10 00:37:35 +0900

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Jamie on 2020/09/10.
+//  Copyright © 2020 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
